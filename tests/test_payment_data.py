9595a17c3e4d1973594b3a776d7e151821b17839
happiemiel <happiemiel@gmail.com>
2020-08-14 22:14:53 +0200

    adding parametrized fixture

--- tests/test_payment_data.py
+++ tests/test_payment_data.py
@@ -7,38 +7,62 @@ import pytest
 from core import PaymentData
 
 
-@pytest.fixture()
-def simple_payment():
+AM = 'amount'
+IP = 'interest'
+RP = 'repayment'
+
+payment_fixture_data = {
+    'reg1': {
+        AM: 1000,
+        IP: 10,
+        RP: 20,
+    },
+    'reg2': {
+        AM: 1000,
+        IP: 50,
+        RP: 50,
+    },
+    'reg3': {
+        AM: 0.,
+        IP: -5.,
+        RP: -10.,
+    },
+}
+
+
+@pytest.fixture(params=list(payment_fixture_data.values()),
+                ids=list(payment_fixture_data.keys()))
+def payment_fixture(request):
     """Fixture payment data will all attributes assigned"""
     payment_data = PaymentData()
-    payment_data.amount_boy = 1000.
-    payment_data.interest = 10.
-    payment_data.repayment = 20
+    payment_data.amount_boy = request.param[AM]
+    payment_data.interest = request.param[IP]
+    payment_data.repayment = request.param[RP]
     return payment_data
 
 
-def test_add_two_payments(simple_payment):
+def test_add_two_payments(payment_fixture):
     """Tests whether all parameters of two payments are added correctly"""
 
     # arrange
     payment2 = PaymentData()
     payment2.interest = 100.
-    expected_interest = payment2.interest + simple_payment.interest
+    expected_interest = payment2.interest + payment_fixture.interest
 
     # act
-    added = simple_payment + payment2
+    added = payment_fixture + payment2
 
     # assert
     assert added.interest == expected_interest
 
 
-def test_payment_property(simple_payment):
+def test_payment_property(payment_fixture):
     """tests whether payment is the sum of interest and repayment"""
 
-    assert simple_payment.payment == simple_payment.interest + simple_payment.repayment
+    assert payment_fixture.payment == payment_fixture.interest + payment_fixture.repayment
 
 
-def test_end_amount(simple_payment):
+def test_end_amount(payment_fixture):
     """tests whether amount end equals start minus repayment"""
 
-    assert simple_payment.amount_end == simple_payment.amount_boy - simple_payment.repayment
+    assert payment_fixture.amount_end == payment_fixture.amount_boy - payment_fixture.repayment
