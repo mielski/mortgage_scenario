8da55f37fdde729a7ff13b0f4f0c5383fd635786
happiemiel <happiemiel@gmail.com>
2020-08-18 21:25:53 +0200

    simplify MANIFEST.in setup.py: - removed older python versions - removed entry points tox: removed py35 to 37

--- setup.py
+++ setup.py
@@ -16,24 +16,16 @@ test_requirements = ['pytest>=3', ]
 setup(
     author="Emiel van IJsseldijk",
     author_email='happiemiel@gmail.com',
-    python_requires='>=3.5',
+    python_requires='==3.8',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
     ],
     description="computes future mortgage payments of combines loanparts and with various interest events",
-    entry_points={
-        'console_scripts': [
-            'mortgage_scenarios=mortgage_scenarios.cli:main',
-        ],
-    },
     install_requires=requirements,
     license="BSD license",
     long_description=readme,
