c3d6b51d14730bc875adc99781316e3513eb9a76
happiemiel <happiemiel@gmail.com>
2020-08-20 09:12:12 +0200

    added two more use tests flake8 and import sorting

--- tests/use_tests/test_simple_loans.py
+++ tests/use_tests/test_simple_loans.py
@@ -2,11 +2,12 @@
 use test with LoanRunner of one or two loans, without additional events such as
 prepayments or interest changes during course of mortgage
 """
-import pytest
-import mortgage_scenarios
-from re import sub
 from io import StringIO
+from re import sub
+
 import pandas as pd
+
+import mortgage_scenarios
 
 
 def read_markdown_string(md_string: str) -> pd.DataFrame:
@@ -21,15 +22,18 @@ def read_markdown_string(md_string: str) -> pd.DataFrame:
     - remove opening and trailing empty lines
     """
 
+    md_string = md_string.replace(" ", "")
     md_string = sub(r'\|?\n\|?', r'\n', md_string)
     md_string = sub(r'[-:|]+\n', '', md_string, count=1)
-    md_string = md_string.replace(" ", "")
     md_string = md_string[1:-1]
     return pd.read_csv(StringIO(md_string), sep="|", index_col=0)
 
 
-@pytest.mark.slow
 def test_vanilla_annuity():
+    """
+    use test of an annuity loan. Given loan specification, the loan is iterated
+    and converted into a dataframe. This dataframe is compared to the expected values
+    """
 
     # arrange
     expected_md = """
@@ -44,6 +48,74 @@ def test_vanilla_annuity():
     periods = 3
     rate = 0.126825
     loan = mortgage_scenarios.LoanPartIterator(amount, rate, periods)
+
+    expected_df = read_markdown_string(expected_md)
+
+    # act
+    runner = mortgage_scenarios.MortgageLoanRunner()
+    runner.add_loanpart(loan)
+    runner.step_all()
+    df = runner.to_dataframe()
+
+    # assert
+    pd.testing.assert_frame_equal(df, expected_df)
+
+
+def test_vanilla_interest_only():
+    """
+    use test of an interest-only loan.
+    Given loan specification, the loan is iterated and converted into a dataframe.
+    This dataframe is compared to the expected values
+    """
+
+    # arrange
+    expected_md = """
+|   period |   amount |   payment |   interest |   repayment |   amount_end |
+|---------:|---------:|----------:|-----------:|------------:|-------------:|
+|        0 | 100.     | 0.9999997 |  0.9999997 | 0.0         | 100.         |
+|        1 | 100.     | 0.9999997 |  0.9999997 | 0.0         | 100.         |
+|        2 | 100.     | 0.9999997 |  0.9999997 | 0.0         | 100.         |
+    """
+
+    amount = 100
+    periods = 3
+    rate = 0.126825
+    fv = 100
+    loan = mortgage_scenarios.LoanPartIterator(amount, rate, periods, fv)
+
+    expected_df = read_markdown_string(expected_md)
+
+    # act
+    runner = mortgage_scenarios.MortgageLoanRunner()
+    runner.add_loanpart(loan)
+    runner.step_all()
+    df = runner.to_dataframe()
+
+    # assert
+    pd.testing.assert_frame_equal(df, expected_df)
+
+
+def test_annuity_plus_fixed_rate():
+    """
+    use test of the previous annuity loan, including a fixed amount.
+    Given loan specification, the loan is iterated and converted into a dataframe.
+    This dataframe is compared to the expected values
+    """
+
+    # arrange
+    expected_md = """
+|   period |   amount |   payment |   interest |   repayment |   amount_end |
+|---------:|---------:|----------:|-----------:|------------:|-------------:|
+|        0 | 100      |   36.0022 |  2.9999997 |     33.0022 | 66.9977      |
+|        1 |  66.9977 |   36.0022 |  2.6699777 |     33.3322 | 33.6655      |
+|        2 |  33.6655 |   36.0022 |  2.3366554 |     33.6655 |  7.10543e-15 |
+    """
+
+    amount = 100
+    periods = 3
+    rate = 0.126825
+    fixed = 2
+    loan = mortgage_scenarios.LoanPartIterator(amount, rate, periods, fixed=fixed)
 
     expected_df = read_markdown_string(expected_md)
 
